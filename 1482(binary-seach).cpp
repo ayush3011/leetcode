f5f9a3e2acf717d000ab6e202bf75c7fdcf56702
Ayush Gangwar <ayushgangwar3011@gmail.com>
2024-06-19 23:49:56 +0530

    Create 1482(binary-seach).cpp

---
+++ 1482(binary-seach).cpp
@@ -0,0 +1,38 @@
+bool isOkayDays(int mid, vector<int> &bloomDay, int m, int k)
+{
+        int consecCount=0, numOfBouquet=0;
+        for(int i=0; i<bloomDay.size(); i++)
+        {
+            if(bloomDay[i]<=mid) consecCount++;
+            else consecCount=0;
+
+            if(consecCount==k)
+            {
+                numOfBouquet++;
+                consecCount=0;
+            }
+        }
+        return (numOfBouquet>=m);
+}
+
+int minDays(vector<int>& bloomDay, int m, int k) 
+{
+        int n=bloomDay.size();
+        if((long)m*k>n) return -1;
+
+        int l=1, r=*max_element(bloomDay.begin(), bloomDay.end()), ans=-1;
+        while(l<=r)
+        {
+            int mid=(l+r)/2;
+            if(isOkayDays(mid, bloomDay, m, k))
+            {
+                ans=mid;
+                r=mid-1;
+            }
+            else
+            {
+                l=mid+1;
+            }
+        }
+        return ans;
+}
