fc564d698fcdbe0111f257c323d1f0324535ed56
Ayush Gangwar <ayushgangwar3011@gmail.com>
2024-06-21 16:38:34 +0530

    Create 1052(sliding-window).cpp

---
+++ 1052(sliding-window).cpp
@@ -0,0 +1,20 @@
+int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) 
+{
+        int windowSum = 0, zeroSum = 0, maxWindowSum = 0, n = customers.size();
+        for (int i = 0; i < n; i++) 
+        {
+            if (grumpy[i] == 0)
+                zeroSum += customers[i];
+            if (i < minutes) 
+            {
+                windowSum += (grumpy[i] == 1 ? customers[i] : 0);
+            } else 
+            {
+                windowSum +=
+                    (grumpy[i] == 1 ? customers[i] : 0) -
+                    (grumpy[i - minutes] == 1 ? customers[i - minutes] : 0);
+            }
+            maxWindowSum = max(maxWindowSum, windowSum);
+        }
+        return maxWindowSum + zeroSum;
+}
