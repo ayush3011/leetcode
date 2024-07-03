4e04ae27476374b74cb312954fe2418956a6fa22
Ayush Gangwar <ayushgangwar3011@gmail.com>
2024-07-03 21:06:52 +0530

    Create 1509.cpp

---
+++ 1509.cpp
@@ -0,0 +1,18 @@
+int minDiff(vector<int> nums) 
+{
+
+  int n = nums.size();
+
+  return min({nums[n - 1] - nums[3], nums[n - 2] - nums[2],
+                    nums[n - 3] - nums[1], nums[n - 4] - nums[0]});
+}
+
+int minDifference(vector<int>& nums) 
+{
+  if (nums.size() <= 4)
+            return 0;
+  
+  sort(nums.begin(), nums.end());
+
+  return minDiff(nums);
+}
