288e0df0d7cca06db2975853a8cea3bd8e9a4573
Ayush Gangwar <ayushgangwar3011@gmail.com>
2024-07-15 22:33:40 +0530

    Create 1572.cpp

---
+++ 1572.cpp
@@ -0,0 +1,11 @@
+int diagonalSum(vector<vector<int>>& mat) {
+        int ans = 0;
+        int n = mat.size();
+        for (int i = 0; i < n; i++) {
+            ans += (mat[i][i] + mat[i][n - 1 - i]);
+        }
+        if (n % 2 != 0) {
+            ans = ans - (mat[n / 2][n / 2]);
+        }
+        return ans;
+}
