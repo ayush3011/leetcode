bfec50d72e3dfc82adc4fd0dfed2d3e5fa7dc885
Ayush Gangwar <ayushgangwar3011@gmail.com>
2024-10-08 17:01:05 +0530

    Create 1963. Minimum Number of Swaps to Make the String Balanced.cpp

---
+++ 1963. Minimum Number of Swaps to Make the String Balanced.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minSwaps(string s) {
+
+        int size = 0;
+
+        for (char c : s) {
+            if (c == '[')
+                size++;
+            else if (size > 0)
+                size--;
+        }
+
+        return (size + 1) / 2;
+    }
+};
