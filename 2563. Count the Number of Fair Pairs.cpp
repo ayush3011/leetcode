ed18dbc005917f3bc456543d33e219371f563db0
Ayush Gangwar <ayushgangwar3011@gmail.com>
2025-04-20 12:01:37 +0530

    Create 2563. Count the Number of Fair Pairs.cpp

---
+++ 2563. Count the Number of Fair Pairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long countFairPairs(vector<int>& nums, int lower, int upper) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        long long result = 0;
+
+        for (int i = 0; i < n; i++) {
+            int left = lower_bound(nums.begin() + i + 1, nums.end(), lower - nums[i]) - nums.begin();
+            int right = upper_bound(nums.begin() + i + 1, nums.end(), upper - nums[i]) - nums.begin();
+            result += (right - left);
+        }
+
+        return result;
+    }
+};
