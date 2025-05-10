1c18a81e07b3303f377eac5b51c1b379f62f4545
Ayush Gangwar <ayushgangwar3011@gmail.com>
2025-05-10 19:33:40 +0530

    Create 2918. Minimum Equal Sum of Two Arrays After Replacing Zeros.cpp

---
+++ 2918. Minimum Equal Sum of Two Arrays After Replacing Zeros.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    long long minSum(vector<int>& nums1, vector<int>& nums2) {
+        long long sum1 = 0;
+        long long sum2 = 0;
+        int zero1 = 0;
+        int zero2 = 0;
+
+        for (auto& num : nums1) {
+            sum1 += num;
+            if (num == 0) {
+                sum1 += 1;
+                zero1++;
+            }
+        }
+
+        for (auto& num : nums2) {
+            sum2 += num;
+            if (num == 0) {
+                sum2 += 1;
+                zero2++;
+            }
+        }
+
+        if (sum1 < sum2 && zero1 == 0) {
+            return -1;
+        }
+        if (sum2 < sum1 && zero2 == 0) {
+            return -1;
+        }
+
+        return max(sum1, sum2);
+    }
+};
