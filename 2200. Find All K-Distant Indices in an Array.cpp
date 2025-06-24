7b1e359aa98bc7b52fc2b64c73c0e24aa65d7e3a
Ayush Gangwar <ayushgangwar3011@gmail.com>
2025-06-24 11:19:00 +0530

    Update 2200. Find All K-Distant Indices in an Array.cpp

--- 2200. Find All K-Distant Indices in an Array.cpp
+++ 2200. Find All K-Distant Indices in an Array.cpp
@@ -1,30 +1,54 @@
 class Solution {
 public:
+    // Brute Force : TC - O(N∗K∗LogN)
+    // vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
+    //     // making a set to store only unique indices
+    //     set<int> s;
+    //     int n = nums.size();
+
+    //     // iterating and finding the key
+    //     for (int i = 0; i < nums.size(); i++) {
+    //         int ele = nums[i];
+    //         if (ele == key) { // if key found, generating all possible
+    //         indices
+    //                           // for the key index
+    //             for (int j = 0; j <= i; j++) {
+    //                 if (abs(i - j) <= k) {
+    //                     s.insert(j);
+    //                 }
+    //             }
+    //             for (int j = i; j < n; j++) {
+    //                 if (abs(i - j) <= k) {
+    //                     s.insert(j);
+    //                 }
+    //             }
+    //         }
+    //     }
+
+    //     // copying the set into vector
+    //     vector<int> ans(s.begin(), s.end());
+    //     return ans;
+    // }
+
+    // Optimized version - TC : O(N) , Spcace : O(N)
     vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
-        // making a set to store only unique indices
-        set<int> s;
+        vector<int> result;
         int n = nums.size();
 
-        // iterating and finding the key
-        for (int i = 0; i < nums.size(); i++) {
-            int ele = nums[i];
-            if (ele == key) { // if key found, generating all possible indices
-                              // for the key index
-                for (int j = 0; j <= i; j++) {
-                    if (abs(i - j) <= k) {
-                        s.insert(j);
-                    }
-                }
-                for (int j = i; j < n; j++) {
-                    if (abs(i - j) <= k) {
-                        s.insert(j);
-                    }
-                }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == key) {
+                int start = max(i - k, 0);
+                int end = min(i + k, n - 1);
+
+                // to avoid duplicates, we start from where the last element of
+                // the vector has been added
+                if (result.size() > 0 && result.back() >= start)
+                    start = result.back() + 1;
+
+                for (int j = start; j <= end; j++)
+                    result.push_back(j);
             }
         }
-
-        // copying the set into vector
-        vector<int> ans(s.begin(), s.end());
-        return ans;
+        return result;
     }
 };
